cf1fe0bfce35e6b1b9d77d188500081a91bfbb16
RoeiBachar <roeibachar972@gmail.com>
2023-01-03 19:01:07 +0200

    images

--- src/Components/NewMain/NewMain.tsx
+++ src/Components/NewMain/NewMain.tsx
@@ -1,53 +1,57 @@
 import { useState } from "react";
 import "./NewMain.css";
-import { questions } from "./questions";
+import { questions} from "./questions";
 
 function NewMain(): JSX.Element {
+     interface questions{
+        text:string,
+        image:string
+       }
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [numCorrectAnswers, setNumCorrectAnswers] = useState(0);
   const [backgroundColor, setBackgroundColor] = useState("");
-  const [corrindex, setcorrindex] = useState(0);
-  const handleAnswer = (answer: any,index:number) => {
+  const handleAnswer = (answer: any) => {
     if (answer === questions[currentQuestionIndex].correctAnswer) {
-      // Increment the number of correct answers if the answer is correct
-      setNumCorrectAnswers(numCorrectAnswers + 1);
       setBackgroundColor("green");
-      setcorrindex(index)
+
+      setNumCorrectAnswers(numCorrectAnswers + 1);
     }
     // Go to the next question
     setCurrentQuestionIndex(currentQuestionIndex + 1);
   };
   return (
     <div className="NewMain">
-      {/* If there are no more questions, show the result message */}
-      {currentQuestionIndex >= questions.length ? (
-        <div>
-          You got {numCorrectAnswers} out of {questions.length} questions
-          correct!
-        </div>
-      ) : (
-        <div>
-          {/* Otherwise, show the current question and options */}
-          <div
-            style={{
-              display: "flex",
-              justifyItems: "center",
-              textAlign: "center",
-              flexDirection: "column",
-            }}
-          >
-            <div  style={{ backgroundColor: `${backgroundColor}` }}>{questions[currentQuestionIndex].quest} </div>
-            {questions[currentQuestionIndex].answers.map((answer,index) => (
-              <button 
-              {index}
-                onClick={() => handleAnswer(answer,index)}
-              >
-                {answer}
-              </button>
-            ))}
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        {currentQuestionIndex >= questions.length ? (
+          <div>
+            You got {numCorrectAnswers} out of {questions.length} questions
+            correct!
           </div>
-        </div>
-      )}
+        ) : (
+          <div>
+            <div
+              style={{
+                display: "flex",
+                justifyItems: "center",
+                textAlign: "center",
+                flexDirection: "column",
+              }}
+            >
+              <div style={{ backgroundColor: `${backgroundColor}` }}>
+                {questions[currentQuestionIndex].quest}{" "}
+              </div>
+              {questions[currentQuestionIndex].answers.map((answer) => (
+                <button onClick={() => handleAnswer(answer.text)}>
+                  <img
+                    src={answer.image}
+                    width={20}
+                  />{answer.text}
+                </button>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
--- src/Components/NewMain/questions.ts
+++ src/Components/NewMain/questions.ts
@@ -1,22 +1,25 @@
+
+
+
 export const questions = [
     {
-      quest: "Who won the World Cup in 2018?",
-      answers: ["France", "Germany", "Spain", "Argentina"],
-      correctAnswer: "France",
+        quest: "Who won the World Cup in 2018?",
+        answers: [{ text: 'France', image: 'https://cdn.britannica.com/82/682-050-8AA3D6A6/Flag-France.jpg' }, {text: 'Germany', image:'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAASIAAACuCAMAAAClZfCTAAAAElBMVEUAAAD/zgDdAADnAADaAAD/2AAtsSEoAAAA+ElEQVR4nO3QMQGAMAAEsYeCf8tIuI0pkZANAAAAAAAAAAAAAAAAAAAAgB8dwm6CoqQoKUqKkqKkKClKipKipCgpSoqSoqQoKUqKkqKkKClKipKipCgpSoqSoqQoKUqKkqKkKClKipKipCgpSoqSoqQoKUqKkqKkKClKipKipCgpSoqSoqQoKUqKkqKkKClKewh7CbsIipKipCgpSoqSoqQoKUqKkqKkKClKipKipCgpSoqSoqQoKUqKkqKkKClKipKipCgpSoqSoqQoKUqKkqKkKClKipKipCgpSoqSoqQoKUqKkqKkKClKipKipCgpSoqSoqQoKUofMGTNC8HkSxoAAAAASUVORK5CYII='}, "Spain", "Argentina"],
+        correctAnswer: "France",
     },
     {
-      quest: "Which team has won the most Champions League titles?",
-      answers: ["Real Madrid", "Barcelona", "AC Milan", "Bayern Munich"],
-      correctAnswer: "Real Madrid",
+        quest: "Which team has won the most Champions League titles?",
+        answers: ["Real Madrid", "Barcelona", "AC Milan", "Bayern Munich"],
+        correctAnswer: "Real Madrid",
     },
     {
-      quest: "Which player has scored the most goals in the Premier League?",
-      answers: [
-        "Wayne Rooney",
-        "Alan Shearer",
-        "Thierry Henry",
-        "Sergio Aguero",
-      ],
-      correctAnswer: "Alan Shearer",
+        quest: "Which player has scored the most goals in the Premier League?",
+        answers: [
+            "Wayne Rooney",
+            "Alan Shearer",
+            "Thierry Henry",
+            "Sergio Aguero",
+        ],
+        correctAnswer: "Alan Shearer",
     },
-  ];
+];
