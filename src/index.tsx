5798d5365633fc3285253ffeaa4756fcb03461a9
RoeiBachar <roeibachar972@gmail.com>
2023-05-05 20:43:38 +0300

    fix some issues

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import NewMain from "./Components/NewMain/NewMain";
 import { BrowserRouter } from "react-router-dom";
 import Routing from "./Components/Routing/Routing";
 
